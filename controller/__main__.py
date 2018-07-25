8ac5301f621fc4717a4d55c7f4df2a202a75e616
Shun Git <kwokshungit@gmail.com>
2018-07-26 01:04:14 +0800

    remove old controller

--- controller/__main__.py
+++
@@ -1,9 +0,0 @@
-from os import getenv
-
-try:
-    from . import Blaster
-    b = Blaster(getenv('BLASTER_SETTINGS'))
-    b.start()
-except KeyboardInterrupt:
-    b.stop()
-    print('\nKeyboard interrupt received, exiting')
