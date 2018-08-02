53aff92255892ad47602be56a91f1939b34661ad
Shun Git <kwokshungit@gmail.com>
2018-08-02 16:38:14 +0800

    add dat.GUI

--- ecran/static/js/app.js
+++ ecran/static/js/app.js
@@ -1,2 +1,10 @@
 const canvas = document.getElementById('ecran');
 const ecran = new Ecran(canvas);
+
+const gui = new dat.GUI({resizable: false});
+gui.close();
+
+let u = new Uniforms(ecran);
+Object.keys(u.limits).forEach((k) => {
+  gui.add(u, k, ...u.limits[k]).onFinishChange((v) => u.update(k, v));
+});
